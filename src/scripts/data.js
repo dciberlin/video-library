474460bc635b4d5ac60ce13eae42b4df4cbe198a
AlexandrMel <melnic.alexandr88@gmail.com>
2019-10-21 13:51:27 +0200

    get videos and images from here https://www.dropbox.com/sh/p0i2lrxxsa6ahdg/AAA1XaCNR9Hkq6MZHbJL1LQFa?dl=0

---
+++ src/scripts/data.js
@@ -0,0 +1,7 @@
+module.exports.data =[
+    {
+        title: "Nick Cave",
+        thumb: "",
+        videurl: ""
+    }
+]
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,55 @@
-// Any code you will write for your website (in the future!) should go here
+let data = [
+    {
+    title: "Nick Cave and the Bas Seeds",
+    thumb: "../images/1.jpeg",
+    videurl: "../images/videos/1.mp4"
+},
+    {
+    title: "Nick Cave and the Bas Seeds",
+    thumb: "../images/2.jpeg",
+    videurl: "../images/videos/1.mp4"
+},
+    {
+    title: "Nick Cave and the Bas Seeds",
+    thumb: "../images/3.jpeg",
+    videurl: "../images/videos/1.mp4"
+},
+    {
+    title: "Nick Cave and the Bas Seeds",
+    thumb: "../images/3.jpeg",
+    videurl: "../images/videos/1.mp4"
+}
+];
+
+generateVideoThumbs = data => {
+    data.forEach(el => {
+        let parent = document.createElement("div");
+        parent.classList.add("video-thumb");
+
+        let title = document.createElement("p");
+        title.innerText = el.title;
+
+        let imgCont = document.createElement("div");
+        imgCont.classList.add("img-cont");
+
+        let img = document.createElement("img");
+        img.setAttribute("src", el.thumb);
+        
+
+        imgCont.appendChild(img)
+        parent.appendChild(imgCont)
+        parent.appendChild(title);
+        parent.setAttribute("onclick", togglePlayer());
+
+        let vidCont = document.querySelector(".cont");
+        vidCont.appendChild(parent);
+    });
+};
+generateVideoThumbs(data);
+togglePlayer = e => {
+    let pl = document.querySelector(".player")
+    pl.classList.toggle("active")
+
+}
+let close = document.querySelector(".close");
+close.addEventListener("click", togglePlayer)
