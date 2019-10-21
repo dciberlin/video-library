6591ceac85156203fb71cf7a9d1736997067de2c
Vasileios Psychas <vaspsych@gmail.com>
2019-10-21 13:50:54 +0200

    get videos and images from here https://www.dropbox.com/sh/p0i2lrxxsa6ahdg/AAA1XaCNR9Hkq6MZHbJL1LQFa?dl=0

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,56 @@
 // Any code you will write for your website (in the future!) should go here
+let data = [
+  {
+    title: "Nick Cave and the Bad Seeds",
+    thumb: "../images/1.png",
+    videoUrl: "../images/videos/1.mp4"
+  },
+  {
+    title: "Radiohead",
+    thumb: "../images/2.png",
+    videoUrl: "../images/videos/2.mp4"
+  },
+  {
+    title: "Fleet Foxes",
+    thumb: "../images/3.png",
+    videoUrl: "../images/videos/3.mp4"
+  },
+  {
+    title: "The National",
+    thumb: "../images/4.png",
+    videoUrl: "../images/videos/4.mp4"
+  }
+];
+
+generateVideoThumbs = data => {
+  data.forEach(el => {
+    let parent = document.createElement("div");
+    parent.classList.add("video-thumb");
+
+    let title = document.createElement("p");
+    title.innerText = el.title;
+
+    let imgCont = document.createElement("div");
+    imgCont.classList.add("img-cont");
+
+    let img = document.createElement("img");
+    img.src = el.thumb;
+
+    imgCont.appendChild(img);
+    parent.appendChild(imgCont);
+    parent.appendChild(title);
+
+    let vidCont = document.querySelector(".cont");
+    vidCont.appendChild(parent);
+  });
+};
+
+generateVideoThumbs(data);
+
+togglePlayer = e => {
+  let pl = document.querySelector(".player");
+  pl.classList.toggle("active");
+};
+
+let close = document.querySelector(".close");
+close.addEventListener("click", togglePlayer);
