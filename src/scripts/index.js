ce61fe18736e809e7ac9a8e42789fc3f7feae333
Vasileios Psychas <vaspsych@gmail.com>
2019-10-21 15:46:24 +0200

    ready made

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,29 +3,91 @@ let data = [
   {
     title: "Nick Cave and the Bad Seeds",
     thumb: "../images/1.png",
-    videoUrl: "../images/videos/1.mp4"
+    videoUrl: "1"
   },
   {
     title: "Radiohead",
     thumb: "../images/2.png",
-    videoUrl: "../images/videos/2.mp4"
+    videoUrl: "2"
   },
   {
     title: "Fleet Foxes",
     thumb: "../images/3.png",
-    videoUrl: "../images/videos/3.mp4"
+    videoUrl: "3"
   },
   {
     title: "The National",
     thumb: "../images/4.png",
-    videoUrl: "../images/videos/4.mp4"
+    videoUrl: "4"
+  },
+  {
+    title: "Nick Cave and the Bad Seeds",
+    thumb: "../images/1.png",
+    videoUrl: "1"
+  },
+  {
+    title: "Radiohead",
+    thumb: "../images/2.png",
+    videoUrl: "2"
+  },
+  {
+    title: "Fleet Foxes",
+    thumb: "../images/3.png",
+    videoUrl: "3"
+  },
+  {
+    title: "The National",
+    thumb: "../images/4.png",
+    videoUrl: "4"
+  },
+  {
+    title: "Nick Cave and the Bad Seeds",
+    thumb: "../images/1.png",
+    videoUrl: "1"
+  },
+  {
+    title: "Radiohead",
+    thumb: "../images/2.png",
+    videoUrl: "2"
+  },
+  {
+    title: "Fleet Foxes",
+    thumb: "../images/3.png",
+    videoUrl: "3"
+  },
+  {
+    title: "The National",
+    thumb: "../images/4.png",
+    videoUrl: "4"
   }
 ];
+
+updateVideo = url => {
+  let src = `../images/videos/${url}.mp4`;
+  let vid = document.createElement("video");
+  vid.src = src;
+  vid.autoplay = true;
+  vid.controls = true;
+  let vidParent = document.querySelector(".video-container");
+  vidParent.appendChild(vid);
+};
+
+togglePlayer = e => {
+  let pl = document.querySelector(".player");
+  pl.classList.toggle("active");
+
+  let vidParent = document.querySelector(".video-container");
+  vidParent.innerHTML = "";
+  let url = e.currentTarget.getAttribute("data-url");
+  if (url) updateVideo(url);
+};
 
 generateVideoThumbs = data => {
   data.forEach(el => {
     let parent = document.createElement("div");
     parent.classList.add("video-thumb");
+    parent.addEventListener("click", togglePlayer);
+    parent.dataset.url = el.videoUrl;
 
     let title = document.createElement("p");
     title.innerText = el.title;
@@ -46,11 +108,6 @@ generateVideoThumbs = data => {
 };
 
 generateVideoThumbs(data);
-
-togglePlayer = e => {
-  let pl = document.querySelector(".player");
-  pl.classList.toggle("active");
-};
 
 let close = document.querySelector(".close");
 close.addEventListener("click", togglePlayer);
